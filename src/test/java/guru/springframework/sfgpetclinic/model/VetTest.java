720ca05e82db603f4d9cd351d9d8a6814dd1f8ba
Sondos-Snd <awachri.sondos@gmail.com>
2023-10-04 09:28:52 +0100

    test class

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/VetTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VetTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getSpecialities() {
+    }
+
+    @Test
+    void setSpecialities() {
+    }
+}
